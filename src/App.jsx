3a0adfad5f70e2f4f01eb8a88804236f786b9587
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-12-02 21:47:17 +0530

    Updated Some Chnages

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='container'>
         <h3>Controller Component Example</h3>
         <input 
           type='text'
           value={name}
           onChange={(e) => setName(e.target.value)}
-          placeholder='please enter your name' />
+          placeholder='please enter your name'
+          className='big-input'
+          />
           
           {/* display value */}
           <p>You Typed : {name}</p>
