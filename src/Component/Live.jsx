92b42c584dcc600eb9c9108bb6ba86d62c3b8b7e
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-12-02 22:19:45 +0530

    update was included

--- src/Component/Live.jsx
+++ src/Component/Live.jsx
@@ -3,19 +3,21 @@ import React, { useState } from "react";
 function Live()
 {
     const [name , setName] = useState("");
-     <div className='container'>
-        <h3>Controller Component Example</h3>
-        <input 
-          type='text'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder='please enter your name'
-          className='big-input'
-          />
-          
-          {/* display value */}
-          <p>You Typed : {name}</p>
-      </div>
+    return(
+        <div className='container'>
+            <h3>Controller Component Example</h3>
+            <input 
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder='please enter your name'
+            className='big-input'
+            />
+            
+            {/* display value */}
+            <p>You Typed : {name}</p>
+        </div>
+    )
 }
 
 export default Live 
