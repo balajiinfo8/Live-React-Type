c135c7ed437b279fcb80d3b586727f13cada137c
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-12-02 22:15:56 +0530

    update was included

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-
+import Live from './Component/Live';
 function App() {
-  const [name , setName] = useState("");
 
   return (
     <>
-      <div className='container'>
-        <h3>Controller Component Example</h3>
-        <input 
-          type='text'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder='please enter your name'
-          className='big-input'
-          />
-          
-          {/* display value */}
-          <p>You Typed : {name}</p>
-      </div>
+     <Live/>
     </>
   );
 }
